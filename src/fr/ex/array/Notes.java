33a05dea89e5fd578f36a26da1e8249c134cfb89
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-28 15:18:05 +0100

    modification des méthodes pour utilisation avec une arrayList

--- src/fr/ex/array/Notes.java
+++ src/fr/ex/array/Notes.java
@@ -1,52 +1,42 @@
 package fr.ex.array;
 
+import java.util.List;
+
 public class Notes {
 
 	public static void main(String[] args) {
 		
-		// Our data
-		int[] StudentMark = {10, 12, 15, 20};
-
-		int min = getMinValue(StudentMark);
-		int max = getMaxValue(StudentMark);
-		double average = getAverage(StudentMark);
-		System.out.println(min);
-		System.out.println(max);
-		System.out.println(average);
 		
-;
 	}
 	// A method to find the minimum value in an array
-	public static int getMinValue(int[] numbers) {
-		int minValue = numbers[0];
-		for (int i = 1; i < numbers.length; i++) {
-			if (numbers[i] < minValue) {
-				minValue = numbers[i];
+	public static int getMinValue(List<Integer> numbers) {
+		int minValue = numbers.get(0);
+		for (int i = 1; i < numbers.size(); i++) {
+			if (numbers.get(i) < minValue) {
+				minValue = numbers.get(i);
 			}
 		}
 		return minValue;
 	}
 	
 	// A method to find the maximum value in an array
-	public static int getMaxValue(int[] numbers) {
-		int maxValue = numbers[0];
-		for (int i = 1; i < numbers.length; i++) {
-			if (numbers[i] > maxValue) {
-				maxValue = numbers[i];
+	public static int getMaxValue(List<Integer> numbers) {
+		int maxValue = numbers.get(0);
+		for (int i = 1; i < numbers.size(); i++) {
+			if (numbers.get(i) > maxValue) {
+				maxValue = numbers.get(i);
 			}
 		}
 		return maxValue;
 	}
 	
 	// A method to find the average value in an array
-	public static double getAverage(int[] numbers) {
-		int TotalSum = 0;
-		int lenArray = numbers.length;
-		for (int i =0; i < lenArray; i++) {
-			TotalSum += numbers[i];
+	public static double getAverage(List<Integer> numbers) {
+		int totalSum = 0;
+		for (int num : numbers) {
+			totalSum += num;
 		}
-		double average = (TotalSum / lenArray);
-		return average;
+		return numbers.isEmpty() ? 0 : (double) totalSum / numbers.size();
 	}
 	
 
