9d566feddbc3deb0b6a67cff06db8aa99521783f
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-28 15:40:14 +0100

    ajout d'une boucle while + condtion d'arret pour gérer de multiples entrées

--- src/fr/ex/array/Students.java
+++ src/fr/ex/array/Students.java
@@ -9,38 +9,48 @@ public class Students {
 		Scanner scan = new Scanner(System.in);
 		Map<String, List<Integer>> studentMarks = new HashMap<>();
 		
-		System.out.println("Enter the first name and last name of the student: ");
-		String firstName = scan.next();
-		String lastName = scan.next();
-		scan.nextLine();
-
-		String fullName = firstName + " " + lastName;
+		// We add a break condition for our while loop
+		boolean addMoreStudents = true;
+	
 		
-		System.out.println("Enter the marks (space separated) for " + fullName);
-		String marks = scan.nextLine();
-		String[] marksArray = marks.split(" ");
+		while (addMoreStudents) {
+			System.out.println("Enter the first name and last name of the student: ");
+			String firstName = scan.next();
+			String lastName = scan.next();
+			scan.nextLine();
+	
+			String fullName = firstName + " " + lastName;
 		
-		List<Integer> marksList = new ArrayList<>();
-		for (String mark: marksArray) {
-			try {
-				int value = Integer.parseInt(mark);
-				marksList.add(value);
-				
-			} catch (NumberFormatException err){
-				System.out.println("Invalid Input.");
+			System.out.println("Enter the marks (space separated) for " + fullName);
+			String marks = scan.nextLine();
+			String[] marksArray = marks.split(" ");
+			
+			List<Integer> marksList = new ArrayList<>();
+			for (String mark: marksArray) {
+				try {
+					int value = Integer.parseInt(mark);
+					marksList.add(value);
+					
+				} catch (NumberFormatException err){
+					System.out.println("Invalid Input.");
+				}
+			
 			}
-		
+				
+			studentMarks.put(fullName, marksList);
+			
+			System.out.println("Do you want to add another studen? (y or n ) ? ");
+			String choice = scan.nextLine().trim().toLowerCase();
+			addMoreStudents = choice.equals("y");
+			
 		}
-		
-		studentMarks.put(fullName, marksList);
-		
-		
 		// Display 
 		System.out.println("\nStudent records:");
         for (Map.Entry<String, List<Integer>> entry : studentMarks.entrySet()) {
+        	List<Integer> marks = entry.getValue();
             System.out.println("Student: " + entry.getKey());
             System.out.println("Marks: " + entry.getValue());
-            System.out.println("Mean: " + Notes.getAverage(marksList));
+            System.out.println("Mean: " + Notes.getAverage(marks)+ "\n");
         }
         
         scan.close();
