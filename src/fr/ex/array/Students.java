a0b395c853461e53ef0dad23f602ac5d8a862ca2
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-29 08:59:28 +0100

    ajout de commentaires

--- src/fr/ex/array/Students.java
+++ src/fr/ex/array/Students.java
@@ -3,8 +3,12 @@ package fr.ex.array;
 import java.util.*;
 
 public class Students {
+	// This our Students class, it contains methods to add
+	// and display Students with their marks and mean.
 	
 	public static List<Integer> getValidMarks(Scanner scan, String fullName){
+		// This method contains a loop to verify if the marks are correct,
+		// and while they are not correct, it display an error message and ask for valid marks:
 		List<Integer> marksList = new ArrayList<>();
 		boolean validMarks = false;
 		
@@ -45,11 +49,13 @@ public class Students {
 	}
 	
 	public static String capitalize(String word) {
+		// This method take a String and capitalize it:
 		if (word == null || word.isEmpty()) return word;
 		return	word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase();
 	}
 	
 	public static void displayStudentInfo(String name, List<Integer> notes) {
+		// This method format the display of each student info:
 		System.out.println("Student: " + name);
 	    System.out.println("Marks: " + notes);
 	    System.out.println("Mean: " + Notes.getAverage(notes)+ "\n");
@@ -57,6 +63,7 @@ public class Students {
 	
 	
 	public static void displaySortedAscending(Map<String, List<Integer>> studentMarks) {
+		// This method is used to sort the student list in ascending order:
 		System.out.println("\nStudents sorted by first name (ascending): ");
 		studentMarks.entrySet().stream()
 			.sorted(Comparator.comparing(entry -> entry.getKey().split(" ")[0]))
@@ -64,6 +71,7 @@ public class Students {
 	}
 	
 	public static void displaySortedDescending(Map<String, List<Integer>> studentMarks) {
+		// This method is used to sort the student list in descending order:
 	    System.out.println("\nStudents sorted by first name (descending):");
 	    studentMarks.entrySet().stream()
 	        .sorted(Comparator.comparing((Map.Entry<String, List<Integer>> entry) -> entry.getKey().split(" ")[0]).reversed())
@@ -72,7 +80,10 @@ public class Students {
 
 	
 	public static void main(String[] args) {
-		
+		// This is our main method, it contains the main logic
+		// of our code. Initiate a scan, a HashMap and a loop to
+		// give the possibility to add more students. The it show
+		// a menu for sorting and display accordingly.
 		Scanner scan = new Scanner(System.in);
 		Map<String, List<Integer>> studentMarks = new HashMap<>();
 		
