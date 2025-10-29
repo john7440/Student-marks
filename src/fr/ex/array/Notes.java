41831e1496ef95bd5a73c5c4921f22ed47be9564
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-29 08:49:36 +0100

    ajout + améliorations de commentaires

--- src/fr/ex/array/Notes.java
+++ src/fr/ex/array/Notes.java
@@ -3,11 +3,13 @@ package fr.ex.array;
 import java.util.List;
 
 public class Notes {
+	// This class is used to store methods that will helps us
+	// to find find any minimum , maximum or average in a List (here for marks of students)
 
 	public static void main(String[] args) {
 		
 	}
-	// A method to find the minimum value in an array
+	// A method to find the minimum value in a List
 	public static int getMinValue(List<Integer> numbers) {
 		int minValue = numbers.get(0);
 		for (int i = 1; i < numbers.size(); i++) {
@@ -18,7 +20,7 @@ public class Notes {
 		return minValue;
 	}
 	
-	// A method to find the maximum value in an array
+	// A method to find the maximum value in a List
 	public static int getMaxValue(List<Integer> numbers) {
 		int maxValue = numbers.get(0);
 		for (int i = 1; i < numbers.size(); i++) {
@@ -29,7 +31,7 @@ public class Notes {
 		return maxValue;
 	}
 	
-	// A method to find the average value in an array
+	// A method to find the average value in a List
 	public static double getAverage(List<Integer> numbers) {
 		int totalSum = 0;
 		for (int num : numbers) {
@@ -38,5 +40,4 @@ public class Notes {
 		return numbers.isEmpty() ? 0 : (double) totalSum / numbers.size();
 	}
 	
-
 }
